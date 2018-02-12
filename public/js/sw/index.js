2ba97b2e61cafa94ad9c5e8295a09f57c09dd434
Riley Rangel <rrangel91@gmail.com>
2018-02-12 15:24:41 -0800

    Update sw fetch event listener to respond from cache if available, and issue a network request if not.

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,35 +1,22 @@
 self.addEventListener('install', event => {
-  const urlsToCache = [
-    '/',
-    'js/main.js',
-    'css/main.css',
-    'imgs/icon.png',
-    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-  ]
-
   event.waitUntil(
     caches.open('wittr-static-v1')
       .then(cache => {
-        cache.addAll(urlsToCache)
+        cache.addAll([
+          '/',
+          'js/main.js',
+          'css/main.css',
+          'imgs/icon.png',
+          'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+          'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+        ])
       })
   )
 })
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request)
-      .then(res => {
-        if (res.status === 404) {
-          return fetch('/imgs/dr-evil.gif')
-        }
-        return res
-      })
-      .catch(err => {
-        console.error(err)
-        return new Response('<h1>No Connection!</h1>', {
-          headers: { 'Content-Type': 'text/html' }
-        })
-      })
+    caches.match(event.request)
+      .then(res => res ? res : fetch(event.request))
   )
 });
