d740a63460b372a37d5d5861d16a79ff83911e8b
Riley Rangel <rrangel91@gmail.com>
2018-02-12 13:17:30 -0800

    Update sw function to respond to 404 requests or on error; L3.14.

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,9 +1,19 @@
-self.addEventListener('fetch', function(event) {
-  if (!event || !event.request || ! event.request.url) return
-
-  if (String(event.request.url).match(/(.jpg)$/)) {
-    event.respondWith(
-      fetch('/imgs/dr-evil.gif')
-    )
-  }
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+        if (res.status === 404) {
+          return new Response('<h1>404: Not Found</h1>', {
+            headers: { 'Content-Type': 'text/html' }
+          })
+        }
+        return res
+      })
+      .catch(err => {
+        console.error(err)
+        return new Response('<h1>No Connection!</h1>', {
+          headers: { 'Content-Type': 'text/html' }
+        })
+      })
+  )
 });
