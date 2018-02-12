5c44e1c48d1536d06ff52f6c5afbeb9508795f41
Riley Rangel <rrangel91@gmail.com>
2018-02-12 14:04:41 -0800

    Adding sw install event registration and handler to cache specified urls.

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,11 +1,27 @@
+self.addEventListener('install', event => {
+  const urlsToCache = [
+    '/',
+    'js/main.js',
+    'css/main.css',
+    'imgs/icon.png',
+    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+  ]
+
+  event.waitUntil(
+    caches.open('wittr-static-v1')
+      .then(cache => {
+        cache.addAll(urlsToCache)
+      })
+  )
+})
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
       .then(res => {
         if (res.status === 404) {
-          return new Response('<h1>404: Not Found</h1>', {
-            headers: { 'Content-Type': 'text/html' }
-          })
+          return fetch('/imgs/dr-evil.gif')
         }
         return res
       })
